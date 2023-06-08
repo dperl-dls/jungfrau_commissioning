d35030455f82b35ba9fd59dba944b267ed94e741
David Perl <david.perl@diamond.ac.uk>
2023-06-08 08:03:28 +0100

    expand welcome doc and remove unused CI

--- src/jungfrau_commissioning/__main__.py
+++ src/jungfrau_commissioning/__main__.py
@@ -1,23 +1,38 @@
+import inspect
 from typing import Callable
 
 import IPython
+from dodal.beamlines import i24
 from traitlets.config import Config
 
 from jungfrau_commissioning.utils.utils import text_colors as col
 
-__all__ = ["main", "hlp"]
+__all__ = ["main", "hlp", "i24"]
 
 welcome_message = f"""
 There are a bunch of available functions. Most of them are Bluesky plans which
-should be run using the syntax {col.CYAN}RE({col.GREEN}plan_name{col.CYAN}){col.ENDC}.
+should be run in the Bluesky RunEngine using the syntax {col.CYAN}RE({col.GREEN}
+plan_name{col.CYAN}){col.ENDC}.
 Some functions can poke devices directly to manipulate them. You can try running
 {col.CYAN}hlp({col.GREEN}function_name{col.CYAN}){col.ENDC} for possible information.
+
+Devices are accessed by functions in the {col.CYAN}i24{col.ENDC} module, for example,
+to get a handle on the vertical goniometer device, you can write:
+
+    {col.BLUE}vgonio = i24.vgonio(){col.ENDC}
+    
+To list all the available devices in the {col.CYAN}i24{col.ENDC} module you can run
+
 """
 
 
 def hlp(arg: Callable | None = None):
+    """When called with no arguments, displays a welcome message. Call it on a
+    function to see documentation for it."""
     if arg is None:
         print(welcome_message)
+    else:
+        print(inspect.getdoc(arg))
 
 
 setup: Config = Config()
