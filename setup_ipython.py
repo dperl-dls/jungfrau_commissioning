2193e5ec17692a52b48ec0f41381855aa44ffb00
David Perl <david.perl@diamond.ac.uk>
2023-06-14 17:06:01 +0100

    improve workspace

--- setup_ipython.py
+++ setup_ipython.py
@@ -6,7 +6,9 @@ from bluesky.run_engine import RunEngine
 from dodal.beamlines import i24
 
 from jungfrau_commissioning.__main__ import hlp, list_devices, list_plans
-from jungfrau_commissioning.plans.rotation_scan_plan import *
+from jungfrau_commissioning.plans import gain_mode as gain_mode_plans
+from jungfrau_commissioning.plans import rotation_scan_plan as rotaion_scan_plans
+from jungfrau_commissioning.plans import zebra as zebra_plans
 from jungfrau_commissioning.plans.zebra import *
 from jungfrau_commissioning.utils.log import set_up_logging_handlers
 from jungfrau_commissioning.utils.utils import text_colors as col
--- src/jungfrau_commissioning/__main__.py
+++ src/jungfrau_commissioning/__main__.py
@@ -1,4 +1,5 @@
 import inspect
+from inspect import getmembers, isfunction
 from typing import Callable
 
 import IPython
@@ -6,6 +7,9 @@ from dodal.beamlines import i24
 from dodal.utils import collect_factories
 from traitlets.config import Config
 
+from jungfrau_commissioning.plans import gain_mode as gain_mode_plans
+from jungfrau_commissioning.plans import rotation_scan_plan as rotation_scan_plans
+from jungfrau_commissioning.plans import zebra as zebra_plans
 from jungfrau_commissioning.utils.utils import text_colors as col
 
 __all__ = ["main", "hlp", "list_devices"]
@@ -30,13 +34,20 @@ example, to get a handle on the vertical goniometer device, you can write:
 
     {col.BLUE}vgonio = i24.vgonio(){col.ENDC}
 
+To list all the available plans, you can run:
+
+    {col.BLUE}list_plans(){col.ENDC}
+
 To list all the available devices in the {col.CYAN}i24{col.ENDC} module you can run:
 
     {col.BLUE}list_devices(){col.ENDC}
 
-To list all the available plans in this package you can run:
+To run a basic default rotation scan, you can execute the following commands:
 
-    {col.BLUE}list_plans(){col.ENDC}
+    {col.BLUE}params = RotationScanParameters.from_file("example_params.json"){col.ENDC}
+    {col.BLUE}plan = get_rotation_scan_plan(params, []){col.ENDC} # <- at this stage, \
+devices are initialised if they aren't already
+    {col.BLUE}RE(plan){col.ENDC} # <- sends the plan to the RunEngine for execution
 """
 
 
@@ -45,8 +56,18 @@ def list_devices():
         print(f"    {col.CYAN}i24.{dev}(){col.ENDC}")
 
 
+def pretty_print_module_functions(mod, indent=0):
+    for f in getmembers(mod, isfunction):
+        print(" " * indent + f"{col.CYAN}{f[0]}(){col.ENDC}")
+
+
 def list_plans():
-    print("Not working right now sorry")
+    print(f"{col.BLUE}gain_mode_plans:{col.ENDC}")
+    pretty_print_module_functions(gain_mode_plans, indent=4)
+    print(f"{col.BLUE}zebra_plans:{col.ENDC}")
+    pretty_print_module_functions(zebra_plans, indent=4)
+    print(f"{col.BLUE}rotation_scan_plans:{col.ENDC}")
+    pretty_print_module_functions(rotation_scan_plans, indent=4)
 
 
 def hlp(arg: Callable | None = None):
